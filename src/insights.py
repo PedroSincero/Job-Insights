b03c48be1f7f394b038cb9d83c3809a427a1f185
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-12-02 15:42:18 -0300

    fix - adicionando requisito 3 - [DONE]

--- src/insights.py
+++ src/insights.py
@@ -29,21 +29,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    result = set()
+    jobs_dict = read(path)
+    for dict in jobs_dict:
+        if dict['industry']:
+            result.add(dict['industry'])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return list(result)
 
 
 def filter_by_industry(jobs, industry):
