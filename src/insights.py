335d5af6bec4b694adc272310c3fa1fd20ae980f
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-12-02 15:59:53 -0300

    fix - adicionando requisito 4 - [DONE]

--- src/insights.py
+++ src/insights.py
@@ -57,21 +57,16 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    result = set()
+    jobs_dict = read(path)
+    for dict in jobs_dict:
+        if dict['max_salary'].isnumeric():
+            result.add(int(dict['max_salary']))
 
-    Must call `read`
+    return max(list(result))
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+# isnumeric
+#  https://www.programiz.com/python-programming/methods/string/isnumeric
 
 
 def get_min_salary(path):
