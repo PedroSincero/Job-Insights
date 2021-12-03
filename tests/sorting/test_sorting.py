be170d4047ef7857f49efa1e7d67ab0293f36591
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-12-03 16:25:07 -0300

    fix - finalizando requisito 10 - [DONE]

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,25 +1,28 @@
 from src.sorting import sort_by
+import pytest
 
-# 'max_salary', 'min_salary', 'date_posted' list_date_posted
+
 list_salary = [
-    {"min_salary": 1000, "max_salary": 3500, "date_posted": "2021-01-01"},
-    {"min_salary": 2000, "max_salary": 6500, "date_posted": "2021-01-01"},
-    {"min_salary": 3000, "max_salary": 8500, "date_posted": "2021-01-01"},
+    {"date_posted": "2021-01-01", "max_salary": 6500, "min_salary": 2000},
+    {"date_posted": "2021-02-01", "max_salary": 3500, "min_salary": 1000},
+    {"date_posted": "2021-03-01", "max_salary": 8500, "min_salary": 3000},
 ]
 
-list_max_salary = [
-    {"min_salary": 5000, "max_salary": 15500, "date_posted": "2021-01-01"},
-    {"min_salary": 4000, "max_salary": 10500, "date_posted": "2021-01-01"},
-    {"min_salary": 3000, "max_salary": 8500, "date_posted": "2021-01-01"},
-]
-list_min_salary = [
-    {"min_salary": 1000, "max_salary": 3500, "date_posted": "2021-01-01"},
-    {"min_salary": 2000, "max_salary": 6500, "date_posted": "2021-01-01"},
-    {"min_salary": 3000, "max_salary": 8500, "date_posted": "2021-01-01"},
-]
+list_min_salary = [list_salary[1], list_salary[0], list_salary[2]]
+list_max_salary = [list_salary[2], list_salary[0], list_salary[1]]
+list_date_posted = [list_salary[2], list_salary[1], list_salary[0]]
 
 
 def test_sort_by_criteria():
-    assert sort_by(list_salary, 'min_salary') == list_min_salary
-    assert sort_by(list_salary, 'max_salary') == list_max_salary
-    assert sort_by(list_salary, 'date_posted') == list_max_salary
+    sort_by(list_salary, "min_salary")
+    assert list_salary == list_min_salary
+    # /n
+    # result = sort_by(list_salary, "min_salary")
+    # assert result == list_min_salary
+    sort_by(list_salary, "max_salary")
+    assert list_salary == list_max_salary
+    sort_by(list_salary, "date_posted")
+    assert list_salary == list_date_posted
+
+    with pytest.raises(ValueError, match="invalid sorting criteria: oi bobo"):
+        sort_by(list_salary, "oi bobo")
